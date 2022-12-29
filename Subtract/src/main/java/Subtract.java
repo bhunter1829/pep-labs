6d2ad8aa2b6eec9cb4a4405107f26a93a0be5f98
Hunter <bhunter1829@gmail.com>
2022-12-29 11:37:25 +0000

    Saved progress on Subtract 2022-12-29 11:37:25 +0000

--- Subtract/src/main/java/Subtract.java
+++ Subtract/src/main/java/Subtract.java
@@ -15,6 +15,9 @@ public class Subtract {
      * @return a minus b
      */
     public double sub(double a, double b){
-        return 0;
+        
+        double total = a - b;
+        
+        return total;
     }
 }
