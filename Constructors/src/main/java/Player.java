996346797536f05c3823b51e361749e160457c63
Hunter <bhunter1829@gmail.com>
2022-12-29 11:53:57 +0000

    Saved progress on Constructors 2022-12-29 11:53:57 +0000

--- Constructors/src/main/java/Player.java
+++ Constructors/src/main/java/Player.java
@@ -32,6 +32,11 @@ public class Player {
      */
     public Player(String playerName){
 
+        this.playerName = playerName;
+
+        this.level = 1;
+
+
     }
 
     /**
@@ -48,6 +53,10 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
+
+        this.playerName = playerName;
+
+        this.level = level;
 
     }
 
