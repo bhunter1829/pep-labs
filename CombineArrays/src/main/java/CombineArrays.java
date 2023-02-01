03f28e77ca0647e2574950c54c3f6cf1117790c5
Hunter <bhunter1829@gmail.com>
2023-02-01 23:14:32 +0000

    Saved progress on CombineArrays 2023-02-01 23:14:32 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -9,6 +9,9 @@ public class CombineArrays {
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        return null;
-    }
+        int[] array1and2 = new int[arr1.length + arr2.length];
+System.arraycopy(arr1, 0, array1and2, 0, arr1.length);
+System.arraycopy(arr2, 0, array1and2, arr1.length, arr2.length);
+   return array1and2;
+} 
 }
