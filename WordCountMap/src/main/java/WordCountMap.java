4206d5283673e72a86d768799be52950fb44a76b
Hunter <bhunter1829@gmail.com>
2023-02-08 15:42:13 +0000

    Saved progress on WordCountMap 2023-02-08 15:42:13 +0000

--- WordCountMap/src/main/java/WordCountMap.java
+++ WordCountMap/src/main/java/WordCountMap.java
@@ -15,7 +15,18 @@ public class WordCountMap {
      * @return a Map (key/value pairs) of all the words mapped to their number of occurrences.
      */
     public Map<String, Integer> returnWordMap(String words){
+        Map<String, Integer> map = new HashMap<String, Integer> ();
+        String[] strings = words.split(" ");
+        for (String s:strings){
+            if(!map.containsKey(s)) {
+                map.put(s, 1);
+            }
+            else{
+                int count = map.get(s);
+                map.put(s, count + 1);
+            }
+        }
 
-        return null;
+        return map;
     }
 }
